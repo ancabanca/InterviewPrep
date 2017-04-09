dc69cd937a42fb15d2d700ba0d5e9dbd8026fe78
Anca Burducea <la.farfelue@gmail.com>
2017-04-09 11:39:11 -0700

    Added red-black balancing logic for BST and a few ordered operations

--- src/util/BST.java
+++ src/util/BST.java
@@ -1,7 +1,11 @@
 package com.github.ancabanca.interviewprep.util;
 
-    
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class BST<K extends Comparable, V> {
+    private static final boolean BLACK = false;
+    private static final boolean RED   = true;
     private Node root;
 
     public V get(K key) {
@@ -23,28 +27,145 @@ public class BST<K extends Comparable, V> {
     }
 
     private Node put(K key, V value, Node n) {
+        if(n == null) return new Node(key, value, RED);
+
+        int cmp = key.compareTo(n.key);
+        if(cmp < 0)       n.left  = put(key, value, n.left);
+        else if (cmp > 0) n.right = put(key, value, n.right);
+        else              n.value = value;
+        n.count = 1 + size(n.left) + size(n.right);
+
+        // red-black BST balancing logic
+        if(isRed(n.right) && !isRed(n.left))     n = rotateLeft(n);
+        if(isRed(n.left)  && isRed(n.left.left)) n = rotateRight(n);
+        if(isRed(n.left)  && isRed(n.right))     flipColors(n);
+
+        return n;
+    }
+
+    private boolean isRed(Node n) {
+        if(n == null) return false;
+        return n.color == RED;
+    }
+
+    private Node rotateLeft(Node n) {
+        Node r = n.right;
+        n.right = r.left;
+        r.left = n;
+        r.color = n.color;
+        n.color = RED;
+        return r;
+    }
+
+    private Node rotateRight(Node n) {
+        Node r = n.left;
+        n.left = r.right;
+        r.right = n;
+        r.color = n.color;
+        n.color = RED;
+        return r;
+    }
+
+    private void flipColors(Node n) {
+        n.left.color  = BLACK;
+        n.right.color = BLACK;
+        n.color = RED;
+    }
+
+    public K min() {
+        Node n = root;
         if(n == null)
-            return new Node(key, value);
+            return null;
+        while(n.left != null)
+            n = n.left;
+        return n.key;
+    }
+
+    public K max() {
+        Node n = root;
+        if(n == null)
+            return null;
+        while(n.right != null)
+            n = n.right;
+        return n.key;
+    }
+
+    public K floor(K key) {
+        Node n = floor(key, root);
+        if(n == null)
+            return null;
+        return n.key;
+    }
+
+    private Node floor(K key, Node n) {
+        if(n == null) return null;
+
+        int cmp = key.compareTo(n.key);
+        if(cmp == 0) return n;
+        if(cmp < 0)  return floor(key, n.left);
+        // else
+        Node f = floor(key, n.right);
+        if(f != null)
+            return f;
+        else
+            return n;
+    }
+
+    public int size() {
+        return size(root);
+    }
+
+    private int size(Node n) {
+        if(n == null)
+            return 0;
+        return n.count;
+    }
+
+    // the number of keys < key
+    public int rank(K key) {
+        return rank(key, root);
+    }
+
+    private int rank(K key, Node n) {
+        if(n == null)
+            return 0;
         int cmp = key.compareTo(n.key);
         if(cmp < 0)
-            n.left = put(key, value, n.left);
-        else if (cmp > 0)
-            n.right = put(key, value, n.right);
+            return rank(key, n.left);
+        else if(cmp > 0)
+            return 1 + size(n.left) + rank(key, n.right);
         else
-            n.value = value;
-        return n;
+            return size(n.left);
+    }
+
+    public Iterable<K> keys() {
+        Queue<K> queue = new LinkedList<K>();
+        inorder(root, queue);
+        return queue;
+    }
+
+    private void inorder(Node n, Queue<K> q) {
+        if(n == null)
+            return;
+        inorder(n.left, q);
+        q.add(n.key);
+        inorder(n.right, q);
     }
 
     private class Node {
         private K key;
         private V value;
         private Node left, right;
+        private int count;
+        private boolean color;
 
-        Node(K key, V value) {
+        Node(K key, V value, boolean color) {
             if(key == null || value == null)
                 throw new IllegalArgumentException("Keys and values in BST cannot be null");
-            this.key = key;
+            this.key   = key;
             this.value = value;
+            this.color = color;
+            this.count = 1;
         }
     }
 }
--- test/BSTTest.java
+++ test/BSTTest.java
@@ -4,9 +4,36 @@ import com.github.ancabanca.interviewprep.util.BST;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class BSTTest {
+    private static BST<String,Integer> bstSmall;
+    private static BST<String,Integer> bst2DigitInts;
+
+    @BeforeClass
+    public static void setup() throws FileNotFoundException {
+        bstSmall = new BST<String,Integer>();
+        String[] strings = "searchexample".split("");
+        Integer[] ints = new Integer[] {7,2,4,9,5,3,7,10,1,6,2,9,12};
+        for(int i = 0; i < ints.length; i++) {
+            bstSmall.put(strings[i], ints[i]);
+        }
+
+        bst2DigitInts = new BST<String,Integer>();
+        Scanner s = new Scanner(new File("test/input/2DigitInts500.txt"));
+        while(s.hasNextLine()) {
+            String key = s.nextLine();
+            Integer count = bst2DigitInts.get(key);
+            if(count == null)
+                bst2DigitInts.put(key, 1);
+            else
+                bst2DigitInts.put(key, count+1);
+        }
+        s.close();
+    }
+
+    // empty tree
     @Test
     public void testBSTEmptyTree() {
         BST<String,Integer> bst = new BST<String,Integer>();
@@ -20,43 +47,67 @@ public class BSTTest {
         assertEquals(5, (int)bst.get("s"));
     }
 
+    // basic operations: put and get
     @Test
     public void testBSTPutGetSmallTree() {
-        BST<String,Integer> bst = new BST<String,Integer>();
-        String[] strings = "searchexample".split("");
-        Integer[] ints = new Integer[] {7,2,4,9,5,3,7,10,1,6,2,9,12};
-        for(int i = 0; i < ints.length; i++) {
-            bst.put(strings[i], ints[i]);
-        }
-        assertEquals(7, (int)bst.get("s"));
-        assertEquals(1, (int)bst.get("a"));
-        assertEquals(12, (int)bst.get("e"));
+        assertEquals(7, (int)bstSmall.get("s"));
+        assertEquals(1, (int)bstSmall.get("a"));
+        assertEquals(12, (int)bstSmall.get("e"));
     }
 
     @Test
-    public void testBSTPutGetRandomInts() throws FileNotFoundException {
-        BST<String,Integer> bst = new BST<String,Integer>();
-        Scanner s = new Scanner(new File("test/input/randomInts500.txt"));
-        while(s.hasNextLine()) {
-            String key = s.nextLine();
-            if(key.length() < 2)
-                continue;
-            key = key.substring(0,2);
-            Integer count = bst.get(key);
-            if(count == null)
-                bst.put(key, 1);
-            else
-                bst.put(key, count+1);
-        }
-        assertEquals(11, (int)bst.get("13"));
-        assertEquals( 4, (int)bst.get("72"));
-        assertNull(bst.get("7"));
+    public void testBSTPutGetRandomInts() {
+        assertEquals(11, (int)bst2DigitInts.get("13"));
+        assertEquals( 4, (int)bst2DigitInts.get("72"));
+        assertNull(bst2DigitInts.get("7"));
     }
 
-    // @Test
-    // public void testBSTInsertEmptyTree() {
-    //     BST<String,Integer> bst = new BST<String,Integer>();
-    //     bst.put("s",5);
-    //     assertEquals(5, (int)bst.get("s"));
-    // }
+    // min and max
+    @Test
+    public void testBSTMinSmallTree() {
+        assertEquals("a", bstSmall.min());
+    }
+
+    @Test
+    public void testBSTMinRandomInts() {
+        assertEquals("10", bst2DigitInts.min());
+    }
+
+    @Test
+    public void testBSTSmallTreeMax() {
+        assertEquals("x", bstSmall.max());
+    }
+
+    @Test
+    public void testBSTRandomIntsMax() {
+        assertEquals("99", bst2DigitInts.max());
+    }
+
+    // floor, size, rank
+    @Test
+    public void testBSTRandomIntsFloor() {
+        assertEquals("55", bst2DigitInts.floor("56"));
+    }
+
+    @Test
+    public void testBSTRandomIntsSize() {
+        assertEquals(71, bst2DigitInts.size()); // FAILS
+    }
+
+    @Test
+    public void testBSTRandomIntsRank() {
+        assertEquals(40, bst2DigitInts.rank("52"));
+    }
+
+    // inorder traversal
+    @Test
+    public void testBSTSmallTreeInorder() {
+        String[] strings = "acehlmprsx".split("");
+        Iterable<String> keys = bstSmall.keys();
+        int i = 0;
+        for(String k : keys) {
+            assertEquals(strings[i++], k);
+        }
+        assertEquals(strings.length, i);
+    }
 }
